3de2b15773caac34ed863e68338bae52e31bbeb0
to0d <to0d@outlook.com>
2023-10-23 21:52:01 +0800

    ch4: add the inliner pass to the pass manager

--- toyc.cpp
+++ toyc.cpp
@@ -119,6 +119,9 @@ int dumpMLIR() {
     if (mlir::failed(mlir::applyPassManagerCLOptions(pm)))
       return 4;
 
+    // Inline all functions into main and then delete them.
+    pm.addPass(mlir::createInlinerPass());
+
     // Add a run of the canonicalizer to optimize the mlir module.
     pm.addNestedPass<mlir::mytoy::FuncOp>(mlir::createCanonicalizerPass());
     if (mlir::failed(pm.run(*module)))
